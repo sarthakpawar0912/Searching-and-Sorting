8e40e94b48f29f6c69d5ecc8b131b2da3e7c827a
sarthakpawar0912 <pawarsr06@gmail.com>
2025-05-19 10:53:32 +0530

    Fixed bugs and updated code

--- src/SEARCHING/BINARY/BinarySearch.java
+++ src/SEARCHING/BINARY/BinarySearch.java
@@ -23,6 +23,7 @@ public class BinarySearch {
         int end = arr.length - 1;
 
         while(start <= end) {
+
             int mid = start + (end - start) / 2;
             if (target < arr[mid]) {
                 end = mid - 1;
